6d58d2b6d260ed1979ed955178c36d9458c4c745
Gutsover <mylenevandaele@gmail.com>
2019-05-25 14:34:49 +0200

    add first commit

---
+++ traitement.php
@@ -0,0 +1,21 @@
+<?php
+//echo "Bonjour " . $_POST['nom'];
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=cvidp;charset=utf8','root','');
+    // Connexion Mysql
+}
+
+catch(Exeption $e) {
+    die('Erreur : ' . $e->getMessage());
+    //Si erreur affiche...
+}
+
+$req = $bdd->prepare('INSERT INTO contact (prenom,nom,email,message) VALUES (?, ?, ?, ?)');
+
+$req->execute(array($_POST['prenom'],$_POST['nom'],$_POST['email'],$_POST['message']));
+
+echo "Votre message a bien était envoyé!";
+
+//$reponse->closeCursor(); //Ferme la connexion SQL
+
+?>
